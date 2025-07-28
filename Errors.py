260713ef0ba542b6fa403a604eea648e0f5a75be
FRANK FAYIA KENDEMAH(Mānāa̍sē Sīsi̍ŋndo̍) <joseph74790@gmail.com>
2025-07-28 04:28:33 -0500

    Create Errors.py

---
+++ Errors.py
@@ -0,0 +1,5 @@
+100357 (P0000): Python Interpreter Error:
+  Worksheet, line 571
+    (summary[f'{stats['table1_alias']}_only_rows_count'])
+                       ^^^^^^^^^^^^
+SyntaxError: f-string: f-string: unmatched '[
