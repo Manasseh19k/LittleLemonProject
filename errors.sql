e7a162df3e06399f12f91c0135f7ced9b293a41c
FRANK FAYIA KENDEMAH(Mānāa̍sē Sīsi̍ŋndo̍) <joseph74790@gmail.com>
2025-07-28 21:19:04 -0500

    Delete errors.sql

--- errors.sql
+++
@@ -1,8 +0,0 @@
-ERROR - Error writing to stage: (1304): 01bdff8b-0001-f1f5-0000-9e5d03f70e0a: 100093 (22000): 01bdff8b-0001-f1f5-0000-9e5d03f70e0a: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
-Error writing to stage table_validation_MDP_only_20250728_155927.csv: (1304): 01bdff8b-0001-f1f5-0000-9e5d03f70e0a: 100093 (22000): 01bdff8b-0001-f1f5-0000-9e5d03f70e0a: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
-
-ERROR - Error writing to stage: (1304): 01bdff8b-0001-f1f5-0000-9e5d03f70e0a: 100093 (22000): 01bdff8b-0001-f1f5-0000-9e5d03f70e0a: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
-Error writing to stage table_validation_MDP_only_20250728_155927.csv: (1304): 01bdff8b-0001-f1f5-0000-9e5d03f70e0a: 100093 (22000): 01bdff8b-0001-f1f5-0000-9e5d03f70e0a: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
-
-ERROR - Error writing to stage: ("Expected bytes, got a 'int' object", 'Conversion failed for column SQL_VALUE with type object')
-Error writing to stage table_validation_mismatches_SQL_vs_MDP_20250728_155927.csv: ("Expected bytes, got a 'int' object", 'Conversion failed for column SQL_VALUE with type object')
