528a1c5bca9786deb5c5ec3cc371de41760df10e
FRANK FAYIA KENDEMAH(Mānāa̍sē Sīsi̍ŋndo̍) <joseph74790@gmail.com>
2025-07-30 04:13:44 -0500

    Update Errors.py

--- Errors.py
+++ Errors.py
@@ -1,1 +1,17 @@
 Function available memory exhausted. Please visit https://docs.snowflake.com/en/developer-guide/stored-procedure/python/procedure-python-profiler for help identifying bottlenecks.
+
+
+ERROR - Error writing to stage: (1304): 01be03f5-0001-f293-0000-9e5d03f9e35a: 100093 (22000): 01be03f5-0001-f293-0000-9e5d03f9e35a: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
+Error writing to stage table_validation_mismatches_SQL_vs_MDP_20250729_104848.csv: (1304): 01be03f5-0001-f293-0000-9e5d03f9e35a: 100093 (22000): 01be03f5-0001-f293-0000-9e5d03f9e35a: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
+
+
+ERROR - Error writing to stage: (1304): 01be03f5-0001-f293-0000-9e5d03f9e37e: 100093 (22000): 01be03f5-0001-f293-0000-9e5d03f9e37e: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
+Error writing to stage table_validation_SQL_only_20250729_104848.csv: (1304): 01be03f5-0001-f293-0000-9e5d03f9e37e: 100093 (22000): 01be03f5-0001-f293-0000-9e5d03f9e37e: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
+
+
+ERROR - Error writing to stage: (1304): 01be03f5-0001-f293-0000-9e5d03f9e3a2: 100093 (22000): 01be03f5-0001-f293-0000-9e5d03f9e3a2: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
+Error writing to stage table_validation_MDP_only_20250729_104848.csv: (1304): 01be03f5-0001-f293-0000-9e5d03f9e3a2: 100093 (22000): 01be03f5-0001-f293-0000-9e5d03f9e3a2: Cannot unload empty string without file format option field_optionally_enclosed_by being specified.
+
+
+ERROR - Error writing to stage: The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()
+Error writing to stage table_validation_summary_SQL_vs_MDP_20250729_104848.csv: The truth value of an array with more than one element is ambiguous. Use a.any() or a.all()
